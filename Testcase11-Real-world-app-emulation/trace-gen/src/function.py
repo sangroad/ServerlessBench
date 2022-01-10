a9fe593170a5bf0b0ff4dc19805537713d8d84cd
Sangwoong Kim <sw.kim@dgist.ac.kr>
2022-01-10 18:04:53 +0900

    Implement test function

---
+++ Testcase11-Real-world-app-emulation/trace-gen/src/function.py
@@ -0,0 +1,26 @@
+import time
+
+def loop(count):
+	for i in range(0, count):
+		for j in range(0, 48000):
+			pass
+
+
+def main(args):
+	# exec time in milliseond scale
+	run_time = args.get('execTime')
+	start = time.time()
+
+	loop(run_time)
+
+	end = time.time()
+	execTime = (end - start) * 1000
+
+	# print exec time in millisecond scale
+	print(execTime)
+
+	return { 'execTime' : execTime }
+
+# for debug
+if __name__ == '__main__':
+	main({ 'execTime' : 0 })
